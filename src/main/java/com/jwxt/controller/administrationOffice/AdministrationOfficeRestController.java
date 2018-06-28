0b38fa236a91296e776f66452c8edfd395e95fce
Blackdogman <superkidchy@qq.com>
2018-06-28 16:26:49 +0800

    添加了一个教导处的restController

---
+++ src/main/java/com/jwxt/controller/administrationOffice/AdministrationOfficeRestController.java
@@ -0,0 +1,26 @@
+package com.jwxt.controller.administrationOffice;
+
+import com.framework.controller.BaseController;
+import com.framework.utils.PrimaryKeyUtil;
+import com.framework.utils.pageUtil.PagedResult;
+import com.jwxt.model.administrationOffice.ClassInfoVo;
+import com.jwxt.model.system.Student;
+import com.jwxt.model.system.StudentVo;
+import com.jwxt.model.system.Teacher;
+import com.jwxt.model.system.TeacherVo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/administrationOfficeController")
+public class AdministrationOfficeRestController extends BaseController {
+
+
+
+}
