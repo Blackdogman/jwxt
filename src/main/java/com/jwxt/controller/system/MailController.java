6e1cdf020aab0e975f70aba2dc1783bf236175e9
Blackdogman <superkidchy@qq.com>
2018-07-03 09:20:54 +0800

    addMail

---
+++ src/main/java/com/jwxt/controller/system/MailController.java
@@ -0,0 +1,10 @@
+package com.jwxt.controller.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mail")
+public class MailController {
+
+}
